a0f6091ffcaad91807b5a268decd59a7f66c4517
Vila Juan Cruz <yunagosh7@gmail.com>
2022-12-30 16:56:12 -0300

    Hice el contenido principal, adaptarlo a celular

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-(() => {
-    const btnsNav = document.querySelectorAll(".nav-btn");
-    const nav = document.querySelector(".nav-links")
-    btnsNav.forEach(btn=> {
-        btn.addEventListener("click",()=> {
-            nav.classList.toggle("active")
+((d) => {
+    const btnsNav = d.querySelectorAll(".nav-btn");
+    const nav = d.querySelector(".nav-links");
+    const modal = d.querySelector(".modal");
+    const links = d.querySelectorAll(".link");
+    for(let i = 0; i < btnsNav.length; i++) {
+        btnsNav[i].addEventListener("click",()=>{
+            nav.classList.toggle("active");
         })
+    }
+
+
+    modal.addEventListener("click",()=>{
+        location.hash = "#";
+        nav.classList.remove("active")
+
     })
     
-})();
+})(document);
